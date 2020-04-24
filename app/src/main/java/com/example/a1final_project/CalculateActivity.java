ad9ed1c22fd46d9cd26e3542b96a3d3a23a28edd
jenniferoribello <jao8@illinois.edu>
2020-04-24 16:17:04 -0500

    input conversions made

--- app/src/main/java/com/example/a1final_project/CalculateActivity.java
+++ app/src/main/java/com/example/a1final_project/CalculateActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class CalculateActivity extends AppCompatActivity {
 
@@ -28,17 +27,80 @@ public class CalculateActivity extends AppCompatActivity {
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int goalpace = Integer.parseInt(goal.getText().toString());
-                System.out.println(goalpace);
-                Intent intent = new Intent(CalculateActivity.this, RestartActivity.class);
-                intent.putExtra("goalPace", goalpace);
-                startActivity(intent);
+                String goalpace1 = goal.getText().toString();
+                String distanceRan = distance.getText().toString();
+                String timeRan = time.getText().toString();
+                if (goalpace1.contains(":") && !(timeRan.contains(":"))) {
+                    String[] goalpace2 = goalpace1.split(":");
+                    //gets the time in minutes as a double
+                    double goalpace = Double.parseDouble(goalpace2[0]) + (Double.parseDouble(goalpace2[1]) / 60);
+                    //creates intent and puts user's input values
+                    Intent intent = new Intent(CalculateActivity.this, RestartActivity.class);
+                    intent.putExtra("goalPace", goalpace);
+                    intent.putExtra("distance", Double.parseDouble(distanceRan));
+                    intent.putExtra("time", Double.parseDouble(timeRan));
+                    //starts the next activity
+                    startActivity(intent);
 //                if (goalpace > distance divided by time) {
 //                    booing.start();
 //                } else {
 //                    cheering.start();
 //                }
-                finish();
+                    finish();
+                } else if (goalpace1.contains(":") && timeRan.contains(":")) {
+                    String[] goalpace2 = goalpace1.split(":");
+                    String[] timeRan1 = timeRan.split(":");
+                    //gets the goalpace in minutes as a double
+                    double goalpace = Double.parseDouble(goalpace2[0]) + (Double.parseDouble(goalpace2[1]) / 60);
+                    //gets the time in minutes as a double
+                    double timeRan2 = Double.parseDouble(timeRan1[0]) + (Double.parseDouble(timeRan1[1]) / 60);
+                    //creates intent and puts user's input values
+                    System.out.println(goalpace);
+                    System.out.println(timeRan);
+                    Intent intent = new Intent(CalculateActivity.this, RestartActivity.class);
+                    intent.putExtra("goalPace", goalpace);
+                    intent.putExtra("distance", Double.parseDouble(distanceRan));
+                    intent.putExtra("time", timeRan2);
+                    //starts the next activity
+                    startActivity(intent);
+//                if (goalpace > distance divided by time) {
+//                    booing.start();
+//                } else {
+//                    cheering.start();
+//                }
+                    finish();
+                } else if (!(goalpace1.contains(":")) && timeRan.contains(":")) {
+                    String[] timeRan1 = timeRan.split(":");
+                    //gets the goalpace in minutes as a double
+                    //gets the time in minutes as a double
+                    double timeRan2 = Double.parseDouble(timeRan1[0]) + (Double.parseDouble(timeRan1[1]) / 60);
+                    //creates intent and puts user's input values
+                    Intent intent = new Intent(CalculateActivity.this, RestartActivity.class);
+                    intent.putExtra("goalPace", Double.parseDouble(goalpace1));
+                    intent.putExtra("distance", Double.parseDouble(distanceRan));
+                    intent.putExtra("time", timeRan2);
+                    //starts the next activity
+                    startActivity(intent);
+//                if (goalpace > distance divided by time) {
+//                    booing.start();
+//                } else {
+//                    cheering.start();
+//                }
+                    finish();
+                } else {
+                    double goalpace = Double.parseDouble(goalpace1);
+                    Intent intent = new Intent(CalculateActivity.this, RestartActivity.class);
+                    intent.putExtra("goalPace", goalpace);
+                    intent.putExtra("distance", Double.parseDouble(distanceRan));
+                    intent.putExtra("time", Double.parseDouble(timeRan));
+                    startActivity(intent);
+                    //                if (goalpace > distance divided by time) {
+                    //                    booing.start();
+                    //                } else {
+                    //                    cheering.start();
+                    //                }
+                    finish();
+                }
             }
         });
     }
